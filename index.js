3f6c10baf3e08b59901594e66fb06d6035a4d0a7
marcelomengech <marcelomengech@gmail.com>
2024-11-28 11:47:21 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
-// Write your code here
+// equation whose multiplied results will equal 62
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+// generates a random integer greater than 0
+const random = (0) * 1000 + 1;
+const num3 = 8;
+const num4 = 2;
+const mod = num3 / num4;
+
+// will return 20 as the highest number in the set
+const numbers  = (5,10,15,16,17,19,20);
+const max = (5,10,15,16,17,19,20);
